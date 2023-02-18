89c495676f21b81453a1e9739a8951042fe5d320
tanyx369 <67994871+tanyx369@users.noreply.github.com>
2023-02-18 13:13:28 +0800

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Question2 extends JComponent
+{
+    public void paintComponent(Graphics g) {
+    
+    	Graphics2D g2 = (Graphics2D) g;
+    	g2.drawLine(15, 0, 15 , 500);
+    	g2.drawLine(0, 450, 500 , 450);
+    	
+    	for(int i = 10 ; i < 450 ; i+= 10) {
+    		g2.drawLine(5, i, 25, i);
+    	}
+    	
+    	g2.setColor(Color.red);
+    	g2.fillRect(50, 219, 20, 231);
+    	g2.setColor(Color.black);
+    	g2.setFont(new Font("Dialog" , Font.BOLD, 12 ));
+    	g2.drawString("January", 35, 470);
+    	
+    	g2.setColor(Color.BLUE);
+    	g2.fillRect(110, 229, 20, 221 );
+    	g2.setColor(Color.black);
+    	g2.drawString("Febraury", 95, 470);
+    	
+    	g2.setColor(Color.green);
+    	g2.fillRect(170, 339 , 20, 111);
+    	g2.setColor(Color.black);
+    	g2.drawString("March", 165, 470);
+    	
+    	g2.setColor(Color.cyan);
+    	g2.fillRect(230, 69, 20, 381);
+    	g2.setColor(Color.black);
+    	g2.drawString("April", 225 , 470);
+    	
+    	g2.setColor(Color.orange);
+    	g2.fillRect(290, 240, 20, 210);
+    	g2.setColor(Color.black);
+    	g2.drawString("May", 288, 470);
+    	
+    	g2.drawString("Month", 505, 450);
+    	
+    	//System.out.println(g2.getFont());
+    }
+    
+    public static void main(String[] args) {
+    	JFrame frame = new JFrame();
+    	frame.setSize(600, 600);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	JLabel label = new JLabel("Billing Amount");
+    	Question2 line = new Question2();
+    	
+    	/*
+    	 JPanel panel = new JPanel();
+    	panel.setLayout(new FlowLayout());
+    	JLabel label1 = new JLabel("January");
+    	JLabel label2 = new JLabel("February");
+    	JLabel label3 = new JLabel("March");
+    	JLabel label4 = new JLabel("April");
+    	JLabel label5 = new JLabel("May");
+    	panel.add(label1);
+    	panel.add(label2);
+    	panel.add(label3);
+    	panel.add(label4);
+    	panel.add(label5);
+    	 */
+    	
+    	frame.add(label, BorderLayout.NORTH);
+    	frame.add(line);
+    	//frame.add(panel, BorderLayout.SOUTH);
+    	frame.setVisible(true);
+    	
+    	
+    }
+}
